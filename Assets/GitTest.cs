9b75b727db2d3649f21618e6fa76937b76e927d7
kdj406 <kdj406@gmail.com>
2016-05-19 00:38:11 +0900

    commit

--- Assets/GitTest.cs
+++ Assets/GitTest.cs
@@ -6,6 +6,7 @@ public class GitTest : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Debug.Log("Test");
+        Debug.Log("Hello KDJ");
 	}
 	
 	// Update is called once per frame
