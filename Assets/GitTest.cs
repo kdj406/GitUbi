9f25aaf6bd8894cb68d01f3c41bb88a731bc207f
kkdj406 <kdj406@gmail.com>
2016-05-19 00:27:51 +0900

    commit2

---
+++ Assets/GitTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GitTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Test");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
