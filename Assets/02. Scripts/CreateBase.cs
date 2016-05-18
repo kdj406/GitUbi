3fb3bde4aeca4ddc18fa7285bb48b4d3d07f5e70
kkdj406 <kdj406@gmail.com>
2016-05-19 01:16:11 +0900

    commit

---
+++ Assets/02. Scripts/CreateBase.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateBase : MonoBehaviour {
+
+    public GameObject Base;
+
+    public static GameObject[] Bases;
+
+	// Use this for initialization
+	void Start () {
+
+        Bases = new GameObject[100];
+        for(int i = 0; i<10; i++) {
+            for(int j = 0; j<10; j ++) {
+                GameObject obj = Instantiate(Base,
+                                    new Vector3(22.5f + 5 * i, 1, -22.5f + 5 * j), new Quaternion()) as GameObject;
+                obj.transform.parent = gameObject.transform;
+                Bases[10 * i + j] = obj;
+
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
